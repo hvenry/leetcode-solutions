9e0800d12205ccfc71b08f32b7e6d395f576d080
hvenry <hvendittelli@gmail.com>
2024-09-15 22:47:05 -0400

    fix: add better explanation to 739.

--- stacks/daily-temperatures/solution.py
+++ stacks/daily-temperatures/solution.py
@@ -18,6 +18,10 @@ naive approach: O(n^2), check each index, and check every index after to see if 
 
 use a monotonic decreasing stack:
 - monotonic: in decreasing order
+- this is see how many times you need to remove a temperature from the stack to make the current temperature the largest 
+    ** THINK: the way we find how many days until there is a hotter day is by counting forward
+       -->  notice how popping a single item from the stack until it is less than or equal the hottest
+            is  ESSENTIALLY counting the distance to a warmer temperature! same idea, just implemented with a stack
 
 - iterate through each element in the array
 - pop elements from the stack as long as the current element is greater than the temperature at the index stored at the top of the stack
@@ -36,6 +40,8 @@ class Solution:
             while stack and temperature > temperatures[stack[-1]]:
                 # when temperature is greater, pop and calculate check previous indexes index difference (to find days where temp is smaller until hotter day which is current)
                 stack_index = stack.pop()
+                # whenever we pop an item from the stack because it is less than the current, we take it's INDEX and say HOW FAR it is from the higher temperature
+                # the index we are adding is REPLACING THE 0 that was currently there (this is because it is now instead the DISTANCE of a hotter day, this value can absolutely just be all zeros!)
                 res[stack_index] = index - stack_index
             stack.append(index)
         return res
