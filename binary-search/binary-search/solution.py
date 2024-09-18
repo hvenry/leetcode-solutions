0dff5fdd6781c145d959e1bdc6a2def375084a4c
hvenry <hvendittelli@gmail.com>
2024-09-17 23:10:36 -0400

    704. binary-search

---
+++ binary-search/binary-search/solution.py
@@ -0,0 +1,37 @@
+from typing import List
+
+
+'''
+given an array fo integers 'nums' which is sorted in ascending order, and
+an integer 'target', write a function to search 'target' in 'nums'.
+
+if 'target' exists, then return its index. otherwise, return -1.
+
+to implement this with O (log n) time complexity, use binary search.
+
+
+how to implement
+- if target is smaller than mid, search lef
+- if target is larger than mid, search right
+'''
+
+class Solution:
+    def search(self, nums: List[int], target: int) -> int:
+        left = 0
+        right = len(nums) - 1
+
+        while left <= right:
+            middle = (left + right) // 2
+
+            # check if target is at middle
+            if target == nums[middle]:
+                return middle
+            # if target is smaller, ignore right half (set right to be middle THIS MOVES RIGHT HALFWAY INWARD)
+            elif target < nums[middle]:
+                right = middle - 1
+            # if target is greater, ignore left half (set left to be middle THIS MOVES LEFT HALFWAY INWARD)
+            else:
+                left = middle + 1
+
+        return -1
+
---
+++ binary-search/binary-search/test_solution.py
@@ -0,0 +1,29 @@
+import unittest
+from solution import Solution
+
+class TestSolution(unittest.TestCase):
+    def setUp(self):
+        self.solution = Solution()
+
+    def test_target_exists(self):
+        self.assertEqual(self.solution.search([1, 2, 3, 4, 5], 3), 2)
+        self.assertEqual(self.solution.search([1, 2, 3, 4, 5], 1), 0)
+        self.assertEqual(self.solution.search([1, 2, 3, 4, 5], 5), 4)
+
+    def test_target_does_not_exist(self):
+        self.assertEqual(self.solution.search([1, 2, 3, 4, 5], 6), -1)
+        self.assertEqual(self.solution.search([1, 2, 3, 4, 5], 0), -1)
+
+    def test_empty_array(self):
+        self.assertEqual(self.solution.search([], 1), -1)
+
+    def test_single_element_array(self):
+        self.assertEqual(self.solution.search([1], 1), 0)
+        self.assertEqual(self.solution.search([1], 2), -1)
+
+    def test_large_numbers(self):
+        self.assertEqual(self.solution.search([10**5, 10**6, 10**7], 10**6), 1)
+        self.assertEqual(self.solution.search([10**5, 10**6, 10**7], 10**8), -1)
+
+if __name__ == '__main__':
+    unittest.main()
