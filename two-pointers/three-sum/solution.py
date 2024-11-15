2b7f002f22490f7fb1807730798a8d56073a7b64
hvenry <hvendittelli@gmail.com>
2024-11-15 16:27:51 -0500

    Revision: 15. three-sum

--- two-pointers/three-sum/solution.py
+++ two-pointers/three-sum/solution.py
@@ -1,79 +1,93 @@
-from typing import List
-
 """
-given an integer array nums, return the triplets [nums[i], nums[j], nums[k]] such that
+Given an integer array nums, return the triplets [nums[i], nums[j], nums[k]] such that
 
-i != j
-i != k
-j != k
-nums[i] + nums[j] + nums[k] = 0
+i != j  |
+i != k  |- may not contain duplicate triplets
+j != k  |
 
-example
+and nums[i] + nums[j] + nums[k] = 0.
 
+Example 1:
 nums = [-1, 0, 1, 2, -1, 4]
-result: [[-1, -1, 2], [-1, 0, 1]]
+returns: [[-1, -1, 2], [-1, 0, 1]]
 
-why:
-nums[0], + nums[1] + nums[2] = (-1) + 0 + 1 = 0  
-nums[1] + nums[2] + nums[4] = 0 + 1 + (-1) = 0      # not distinct from above
+nums[0] + nums[1] + nums[2] = (-1) + 0 +   1  = 0
+nums[1] + nums[2] + nums[4] =   0  + 1 + (-1) = 0   <- not distinct from above
 nums[0] + nums[3] + nums[4] = (-1) + 2 + (-1) = 0
 
 the distinct triplets are: [-1, 0, 1], and [-1, -1, 2]
 
+Naive Approach:
+- Use a triple for loop, go to each index "a' and look for a "b" + "c" that will sum to a.
+- this will give us a lot of duplicates and a time complexity of O(n^3)
 
-** similar to two-sum II (this uses the two-pointers technique) **
-
-
-first approach: use a triple for loop. (this is not good, we will find lots of duplicates)
-- go to each index "a' and look for a "b" + "c" that will sum to a,
-
-Time: O(n^3)
-
-better approach: sort the input array
+Solution:
+- sort the input array
 - if left neighbor of next number is the same, skip (skips the duplicate)
-- go next, and then find the 2 numbers that sum to current (use two pointers technique learned from two sum II)
+- iterate through the remainder of the array and find the 2 numbers that sum to current (similar to two sum ii)
     - shift left pointer in if the current sum is less than target, shift right pointer in if the current sum is larger than target
-    - also skip over previously 
+    - also skip over previously used numbers
+    - whenever we the difference of the 2 values is 0, we store it as an answer
+    - we keep doing thins until left and right intersect, storing any answers found along the way
 
-    
-    Time Complexity
-sorting: O(n log n)
-two pointers for n numbers: O(n^2)
-total: O(nlogn) + O(n^2) = O(n^2)
+Visualization:
+    [-3, -3, -2, -1, 0, 1, 2, 2, 3]
+      i   <---------------------->
+         left and right search this
 
-    Space Complexity
-O(1) -> sometimes sorting can take O(n) space
-
+    [-3, -3, -2, -1, 0, 1, 2, 2, 3]
+      i              l           r    -> this is a valid solution [-3, 0, 3]
 
 
+    [-3, -3, -2, -1, 0, 1, 2, 2, 3]
+      i                 l     r       -> this is another valid solution [-3, 1, 2]
+
+- Our answer so far (after just checking ith number) is [[-3, 0, 3], [-3, 1, 2]]
+
+- We then continue at the next unique index starting like this:
+    [-3, -3, -2, -1, 0, 1, 2, 2, 3]
+              i   l              r
+
+
+Complexity:
+- Time: O(n^2)
+    - sorting: O(n log n)
+    - two pointers for n numbers: O(n^2)
+- Space: O(1) (assuming sorting in place)
 """
+
+from typing import List
 
 
 class Solution:
     def threeSum(self, nums: List[int]) -> List[List[int]]:
-        res = []
         nums.sort()
+        n = len(nums)
+        res = []
 
-        for i, a in enumerate(nums):
-            # same value as before, skip
-            if i > 0 and a == nums[i - 1]:
+        for i in range(n):
+            if nums[i] > 0:
+                break
+            # skip duplicates
+            elif i > 0 and nums[i] == nums[i - 1]:
                 continue
 
-            left = i + 1
-            right = len(nums) - 1
+            left, right = i + 1, n - 1
 
             while left < right:
-                threeSum = a + nums[left] + nums[right]
-                # move pointers inwards based on if we are above or below the target value
-                if threeSum > 0:
-                    right -= 1
-                elif threeSum < 0:
+                sum = nums[i] + nums[left] + nums[right]
+                if sum == 0:
+                    res.append([nums[i], nums[left], nums[right]])
+                    left, right = left + 1, right - 1
+
+                    # make suer neither are on the same value
+                    while left < right and nums[left] == nums[left - 1]:
+                        left += 1
+                    while left < right and nums[right] == nums[right + 1]:
+                        right -= 1
+                elif sum < 0:
                     left += 1
                 else:
-                    res.append([a, nums[left], nums[right]])
-                    left += 1
-                    # only move left pointer, conditions above will update the right pointer (shift left until it is unique)
-                    while nums[left] == nums[left - 1] and left < right:
-                        left += 1
+                    right -= 1
 
         return res
--- two-pointers/three-sum/test_solution.py
+++ two-pointers/three-sum/test_solution.py
@@ -2,16 +2,29 @@ import unittest
 from solution import Solution
 
 
-class TestSolution(unittest.TestCase):
+class TestThreeSum(unittest.TestCase):
     def setUp(self):
         self.solution = Solution()
 
-    def test(self):
-        nums = [-1, 0, 1, 2, -1, 4]
-        ans = [[-1, -1, 2], [-1, 0, 1]]
+    def test_example_case(self):
+        nums = [-1, 0, 1, 2, -1, -4]
+        expected = [[-1, -1, 2], [-1, 0, 1]]
+        self.assertEqual(self.solution.threeSum(nums), expected)
 
-        res = self.solution.threeSum(nums)
-        self.assertEqual(res, ans)
+    def test_no_triplets(self):
+        nums = [1, 2, -2, -1]
+        expected = []
+        self.assertEqual(self.solution.threeSum(nums), expected)
+
+    def test_all_zeros(self):
+        nums = [0, 0, 0, 0]
+        expected = [[0, 0, 0]]
+        self.assertEqual(self.solution.threeSum(nums), expected)
+
+    def test_multiple_triplets(self):
+        nums = [-2, 0, 1, 1, 2]
+        expected = [[-2, 0, 2], [-2, 1, 1]]
+        self.assertEqual(self.solution.threeSum(nums), expected)
 
 
 if __name__ == "__main__":
