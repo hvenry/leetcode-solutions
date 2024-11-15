87fb7ab6805f0ca988a3cf1224ef919d746b236a
hvenry <hvendittelli@gmail.com>
2024-11-15 15:26:44 -0500

    Revision: 167. two-sum-ii

--- two-pointers/two-sum-ii/solution.py
+++ two-pointers/two-sum-ii/solution.py
@@ -1,38 +1,60 @@
+"""
+Given a 1-indexed array of integers numbers that is already sorted in non-decreasing order, find two
+numbers such that they add up to a specific target number. Let these two numbers be numbers[index_i]
+and numbrs[index_2], where 1 <= index_1 < index_2 <= numbers.length.
+
+Return the indices of the two numbers, index_1 and index_2 added by one as an integer [index_i, index_2]
+of length 2.
+
+There is always exactly 1 solution, you may not use the same element twice.
+
+Example 1:
+numbers = [2, 7, 11, 15]
+target = 9
+returns: [1, 2]
+
+The sum of 2 and 7 is 9, therefore, index_1 = 1, index_2 = 2.
+
+Example 2:
+numbers = [2, 3 4]
+target = 6
+returns: [1, 3]
+
+2 + 4 = 6.
+
+Solution:
+- Since array is sorted, use two pointers technique to find the two indexes that add up to our target sum in a sorted list.
+- We can do this by assigning 2 pointers, one at index 0 (left) and one at the final index (right), and then shift them inside
+  the array one at a time.
+    - We move the left pointer up the array if our sum needs to be larger (we need to add larger numbers, we have not reached target)
+    - We move the right pointer down the array if our sum needs to be smaller (we need to add smallers numbers, we overshot)
+
+- if the pointers intersect, return nothing
+- if target sum is reached, return index of left, and index of right.
+
+Complexity:
+    Time: O(n)
+    Space: O(1)
+"""
+
 from typing import List
-
-"""
-use two pointers technique to find the two indexes that add up
-to our target sum in a sorted list
-
-we can do this by assigning 2 pointers, one at index 0 (left) and one 
-at the final index (right), and then shift them inside the array one at a
-time.
-
-we move the left pointer up the array if our sum needs to be larger
-(since the array is increasing in size)
-
-we move the right pointer down the array if our sum needs to be smaller
-(same reason as above)
-
-if the pointers intersect, return nothing
-if target sum is reached, return index of left, and index of right.
-"""
 
 
 class Solution:
     def twoSum(self, numbers: List[int], target: int) -> List[int]:
+        n = len(numbers)
         left = 0
-        right = len(numbers) - 1
+        right = n - 1
 
+        # go until our pointers intersect
         while left < right:
             sum = numbers[left] + numbers[right]
 
             if sum == target:
                 return [left + 1, right + 1]
-
-            # if sum < target: we want to increase it, by moving left pointer
             if sum < target:
                 left += 1
-            # else, if sum is >= target, we want to lower it by moving the right pointer
             else:
                 right -= 1
+
+        return []
--- two-pointers/two-sum-ii/test_solution.py
+++ two-pointers/two-sum-ii/test_solution.py
@@ -2,17 +2,34 @@ import unittest
 from solution import Solution
 
 
-class TestSolution(unittest.TestCase):
+class TestTwoSumII(unittest.TestCase):
     def setUp(self):
         self.solution = Solution()
 
-    def test(self):
+    def test_example1(self):
         numbers = [2, 7, 11, 15]
         target = 9
-        ans = [1, 2]
+        self.assertEqual(self.solution.twoSum(numbers, target), [1, 2])
 
-        res = self.solution.twoSum(numbers, target)
-        self.assertEqual(res, ans)
+    def test_example2(self):
+        numbers = [2, 3, 4]
+        target = 6
+        self.assertEqual(self.solution.twoSum(numbers, target), [1, 3])
+
+    def test_example3(self):
+        numbers = [-1, 0]
+        target = -1
+        self.assertEqual(self.solution.twoSum(numbers, target), [1, 2])
+
+    def test_example4(self):
+        numbers = [1, 2, 3, 4, 4, 9, 56, 90]
+        target = 8
+        self.assertEqual(self.solution.twoSum(numbers, target), [4, 5])
+
+    def test_example5(self):
+        numbers = [5, 25, 75]
+        target = 100
+        self.assertEqual(self.solution.twoSum(numbers, target), [2, 3])
 
 
 if __name__ == "__main__":
