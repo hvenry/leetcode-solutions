cf0b928376e6d6eca306fb1ab435c545f3e00332
hvenry <hvendittelli@gmail.com>
2024-11-18 18:22:12 -0500

    Revision: 271. contains-duplicate

--- hashmaps-and-sets/contains-duplicate/solution.py
+++ hashmaps-and-sets/contains-duplicate/solution.py
@@ -1,26 +1,34 @@
-from typing import List
+"""
+Given an integer array nums, return true if any value appears at least twice in the array,
+and return false if every element is distinct.
 
+Example 1:
+nums = [1, 2, 3, 1]
+returns: True
+
+Example 2:
+nums = [1, 2, 3, 4]
+returns False
+
+Solution:
+- use a hash set (set) to add each umber of nums to
+- before adding an number to the set, see if nums is already in it
+- if nums is in it, return True, if all elements are seen and no elements have been seen before, return False
+
+Complexity
+- Time: O(n)
+- Space: O(n)
 """
-use a set to see if there is a duplicate
-"""
+
+from typing import List
 
 
 class Solution:
     def containsDuplicate(self, nums: List[int]) -> bool:
         num_set = set()
 
-        for i in nums:
-            if i in num_set:
+        for num in nums:
+            if num in num_set:
                 return True
-            num_set.add(i)
+            num_set.add(num)
         return False
-
-        # or
-        # for i in range(len(nums)):
-        #   if nums[i] in num_set:
-        #     return True
-        #   num_set.add(nums[i])
-        # return False
-
-    # time: O(n)
-    # space: O(n)
--- hashmaps-and-sets/contains-duplicate/test_solution.py
+++ hashmaps-and-sets/contains-duplicate/test_solution.py
@@ -2,16 +2,30 @@ import unittest
 from solution import Solution
 
 
-class TestSolution(unittest.TestCase):
+class TestContainsDuplicate(unittest.TestCase):
     def setUp(self):
         self.solution = Solution()
 
-    def test(self):
-        nums = [1, 2, 3, 1]
-        ans = True
+    def test_example1(self):
+        self.assertTrue(self.solution.containsDuplicate([1, 2, 3, 1]))
 
-        res = self.solution.containsDuplicate(nums)
-        self.assertEqual(res, ans)
+    def test_example2(self):
+        self.assertFalse(self.solution.containsDuplicate([1, 2, 3, 4]))
+
+    def test_empty_list(self):
+        self.assertFalse(self.solution.containsDuplicate([]))
+
+    def test_single_element(self):
+        self.assertFalse(self.solution.containsDuplicate([1]))
+
+    def test_all_duplicates(self):
+        self.assertTrue(self.solution.containsDuplicate([2, 2, 2, 2]))
+
+    def test_large_input(self):
+        self.assertTrue(self.solution.containsDuplicate(list(range(10000)) + [9999]))
+
+    def test_no_duplicates(self):
+        self.assertFalse(self.solution.containsDuplicate(list(range(10000))))
 
 
 if __name__ == "__main__":
