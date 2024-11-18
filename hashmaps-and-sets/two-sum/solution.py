fdd5cfbf80a54e5ad654c6239991b1db13cf539a
hvenry <hvendittelli@gmail.com>
2024-11-18 18:39:34 -0500

    Revision: 1. two-sum

--- hashmaps-and-sets/two-sum/solution.py
+++ hashmaps-and-sets/two-sum/solution.py
@@ -1,36 +1,45 @@
-from typing import List
-
 """
+Given an array of integers nums and an integer target, return indices of the two numbers such
+that they add up to target.
+
+You may assume that each input would have exactly one solution, and you may not use the same element
+twice.
+
+You can return the answer in any order.
+
 nums = [2, 7, 11, 15]
 target = 9
 
 expected: [0, 1]
 
-how to solve with hashmap:
-
+Solution:
+- to get better than O(n^2) time, use a hashmap
 - go through every num in nums
-      print(hashmap)
 - check if matching (which is target - num) is in our hashmap
 - if it is in hashmap, return array [ hashmap[matching] , num ]
 
-example:
-
+Visualization:
 hashmap = {}
 
-iteration 1:
+i = 0:
 num = 2
 matching = 9 - 2 = 7
 hashmap = { 2 : 0 }
 
-iteration 2:
+iteration 1:
 num = 7
 matching = 9 - 7 = 2
-
 matching IS in hashmap ( { 2 : 0 } )
-we now return [ 0 (comes from hashmap[matching] or hashmap[2]), 1 (comes from i)]
 
+we now return [ 0 (comes from hashmap[matching] or hashmap[2]), 1 (comes from i)]
 res = [0, 1]
+
+Complexity:
+- Time: O(n)
+- Space: O(n)
 """
+
+from typing import List
 
 
 class Solution:
@@ -44,6 +53,3 @@ class Solution:
                 return [hashmap[matching], i]
             hashmap[nums[i]] = i
         return []
-
-    # time: O(n)
-    # space: O(n)
--- hashmaps-and-sets/two-sum/test_solution.py
+++ hashmaps-and-sets/two-sum/test_solution.py
@@ -1,20 +1,27 @@
 import unittest
-import unittest.main
 from solution import Solution
 
-
-class TestSolution(unittest.TestCase):
+class TestTwoSum(unittest.TestCase):
     def setUp(self):
         self.solution = Solution()
 
-    def test(self):
-        nums = [2, 7, 11, 15]
-        target = 9
-        ans = [0, 1]
+    def test_example_case(self):
+        self.assertEqual(self.solution.twoSum([2, 7, 11, 15], 9), [0, 1])
 
-        res = self.solution.twoSum(nums, target)
-        self.assertEqual(res, ans)
+    def test_case_with_negative_numbers(self):
+        self.assertEqual(self.solution.twoSum([-3, 4, 3, 90], 0), [0, 2])
 
+    def test_case_with_multiple_solutions(self):
+        self.assertEqual(self.solution.twoSum([1, 2, 3, 4, 5], 6), [1, 3])
 
-if __name__ == "__main__":
+    def test_case_with_large_numbers(self):
+        self.assertEqual(self.solution.twoSum([1000000, 500000, -1500000], -1000000), [1, 2])
+
+    def test_case_with_duplicates(self):
+        self.assertEqual(self.solution.twoSum([3, 3], 6), [0, 1])
+
+    def test_case_with_no_solution(self):
+        self.assertEqual(self.solution.twoSum([1, 2, 3], 7), [])
+
+if __name__ == '__main__':
     unittest.main()
