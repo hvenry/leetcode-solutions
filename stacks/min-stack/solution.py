e603f284575846916c5bb47187205028e6e6208b
hvenry <hvendittelli@gmail.com>
2024-11-20 18:36:01 -0500

    Revision: 155. min-stack

--- stacks/min-stack/solution.py
+++ stacks/min-stack/solution.py
@@ -1,52 +1,42 @@
 """
-design a stack that supports push, pop, top, and retrieving
-the minimum element in constant time
+Design a stack that supports push, pop, top, and retrieving the minimum element in constant time.
 
-MinStack()
-- initializes the stack object
+Implement the MinStack class:
+- MinStack(): initializes the stack object
+- void push(int val): pushes the element val onto the stack
+- void pop(): removes the element on the top of the stack
+- int top(): gets the top element of the stack
+- int getMin(): retrieves the minimum element in the stack
 
-void push(int val)
-- pushes the element val onto the stack
-
-void pop()
-- removes the element on the top of the stack
-
-int top()
-- gets teh top element of the stack
-
-int getMin()
-- retrieves the minimum element in the stack
-
-example
-Input:
+Example:
 ["MinStack","push","push","push","getMin","pop","top","getMin"]
 [[],[-2],[0],[-3],[],[],[],[]]
 
-Result:
-[null,null,null,null,-3,null,0,-2]
+returns: [null,null,null,null,-3,null,0,-2]
 
-Explanation
+Explanation:
 MinStack minStack = new MinStack();
 minStack.push(-2);
 minStack.push(0);
 minStack.push(-3);
-minStack.getMin(); // return -3
+minStack.getMin();                  // return -3
 minStack.pop();
-minStack.top();    // return 0
-minStack.getMin(); // return -2
-
+minStack.top();                     // return 0
+minStack.getMin();                  // return -2
 
 
 Solution:
-stacks can already add a value, pop a value, get top value in O(1) time
+- Stacks can already add a value, pop a value, get top value in O(1) time.
+- To implement min_value with with O(n) time, just go through all numbers and track a max value.
 
-how can we get min value in O(1) time?
-- to implement with O(n) time, just go through all numbers and track a max value
+    - O(1) IMPROVEMENT: Track the min value inside of the stack object WITH ANOTHER STACK
+        - if there is no min_stack, set the min to val
+        - if there is a min_stack, set the min to min(val, self.min_stack[-1]
+        - we can now get the min value by popping the min_stack, O(1) time
 
-better implementation:
-track the min value inside of the stack object WITH ANOTHER STACK
-- the stack gets pushed the minimum value so far, if the next value added to the stack is smaller, then add that to the min stack
-- now we can pop it (get the minimum value) in O(1) time
+Complexity
+- Time: O(1)
+- Space: O(n) -> size of stack
 """
 
 
