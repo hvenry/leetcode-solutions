3335932040682d11dee5d8b35e3098f162683224
hvenry <hvendittelli@gmail.com>
2024-11-21 16:19:32 -0500

    Revision: 739. daily-temperatures

--- stacks/daily-temperatures/solution.py
+++ stacks/daily-temperatures/solution.py
@@ -1,47 +1,96 @@
-from typing import List
-
 """
-given an array of integers 'temperatures', return an array 'answer'
-such that answer[i] is the number of days you have to wait after the ith day
-to get a warmer temperature.
+Given an array of integers temperatures, return an array answer such that answer[i] is the number of
+days you have to wait after the ith day to get a warmer temperature. If there is no future day, set
+answer[i] to 0.
 
-if there is no future day, set answer[i] to 0.
-
-example:
+Example:
 temperatures = [73, 74, 75, 71, 69, 72, 76, 73]
-result: [1, 1, 4, 2, 1, 1, 0, 0]
+returns: [1, 1, 4, 2, 1, 1, 0, 0]
 
-for temperatures[0] you need to wait 1 day for a warmer temperature.
-for temperatures[6] there is no day that gets warmer, so 0.
+- for temperatures[0] you need to wait 1 day for a warmer temperature.
+- for temperatures[6] there is no day that gets warmer, so 0.
 
-naive approach: O(n^2), check each index, and check every index after to see if there is a day that is warmer.
+Naive approach:
+O(n^2), check each index, and check every index after to see if there is a day that is warmer.
 
-use a monotonic decreasing stack:
-- monotonic: in decreasing order
-- this is see how many times you need to remove a temperature from the stack to make the current temperature the largest 
-    ** THINK: the way we find how many days until there is a hotter day is by counting forward
-       -->  notice how popping a single item from the stack until it is less than or equal the hottest
-            is  ESSENTIALLY counting the distance to a warmer temperature! same idea, just implemented with a stack
+Solution;
+- use a monotonic decreasing stack (items are always in decreasing (or equal) order):
+- the stack tracks how many times you need to remove a temperature from the to make the current temp higher
+    - popping a single item from the stack until it is larger will give us the amount of days until a hotter temp
+      is reached.
 
-- iterate through each element in the array
-- pop elements from the stack as long as the current element is greater than the temperature at the index stored at the top of the stack
-- push the current index onto the stack
+Visualization [73, 74, 75, 71, 69, 72, 76, 73]:
+
+index 0, temp 73:
+
+res   = [0, 0, 0, 0, 0, 0, 0]
+stack = [(73, 0)]
+
+
+index 1, temp 74:
+    since temp > stack -1, we pop from stack, and calculate new index - stack_index (1 - 0 = 1)
+    res   = [1, 0, 0, 0, 0, 0, 0, 0]
+    stack = [(74, 1)]
+
+index 2, temp 75:
+    since temp > stack -1 we pop from stack, and calculate index - stack_index (2 - 1 = 1)
+
+    res   = [1, 1, 0, 0, 0, 0, 0, 0]
+    stack = [(75, 2)]
+
+index 3, temp 71:
+    res   = [1, 1, 0, 0, 0, 0, 0, 0]
+    stack = [(75, 2), (71, 3)]
+
+index 4, temp 69:
+    res   = [1, 1, 0, 0, 0, 0, 0, 0]
+    stack = [(75, 2), (71, 3), (69, 4)]
+
+index 5, temp 72:
+    since temp > stack -1, we pop from stack, and calculate index - stack index (5 - 4 = 1)
+    res   = [1, 1, 0, 0, 1, 0, 0, 0]
+    stack = [(75, 2), (71, 3)]
+
+    since temp is still > stack - 1, we pop from stack, and calculate index - stack index (5 - 3 = 2)
+    res   = [1, 1, 0, 2, 1, 0, 0, 0]
+    stack = [(75, 2), (72, 5)]
+
+index 6, temp 76:
+    since temp > stack -1, we pop from stack, and calculate index - stack index (6 - 5 = 1)
+    res   = [1, 1, 4, 2, 1, 1, 0, 0]
+    stack = [(75, 2)]
+
+    since temp > stack -1, we pop from stack, and calculate index - stack index (6 - 2 = 4)
+    res   = [1, 1, 4, 2, 1, 1, 0, 0]
+    stack = [(76, 6)]
+
+index 7, temp 73:
+    res   = [1, 1, 4, 2, 1, 1, 0, 0]
+    stack = [(76, 6), (73, 7)]
+
+Nothing left to do, return answer!
+
+
+Complexity:
+- Time: O(n)
+- Space: O(n)
 """
+
+from typing import List
 
 
 class Solution:
     def dailyTemperatures(self, temperatures: List[int]) -> List[int]:
-        res = [0] * len(temperatures)
-        # store indices of temperatures
+        n = len(temperatures)
+        res = [0] * n
         stack = []
 
-        for index, temperature in enumerate(temperatures):
-            # check if the current temperature is greater than the temperature at the index stored in the stack
-            while stack and temperature > temperatures[stack[-1]]:
-                # when temperature is greater, pop and calculate check previous indexes index difference (to find days where temp is smaller until hotter day which is current)
-                stack_index = stack.pop()
-                # whenever we pop an item from the stack because it is less than the current, we take it's INDEX and say HOW FAR it is from the higher temperature
-                # the index we are adding is REPLACING THE 0 that was currently there (this is because it is now instead the DISTANCE of a hotter day, this value can absolutely just be all zeros!)
-                res[stack_index] = index - stack_index
-            stack.append(index)
+        for i, temp in enumerate(temperatures):
+            # pop from stack until we get a temp that is higher again (maintain monotonic decreasing stack)
+            while stack and temp > stack[-1][0]:
+                _, stack_i = stack.pop()
+                res[stack_i] = i - stack_i
+
+            stack.append((temp, i))
+
         return res
--- stacks/daily-temperatures/test_solution.py
+++ stacks/daily-temperatures/test_solution.py
@@ -2,16 +2,39 @@ import unittest
 from solution import Solution
 
 
-class testSolution(unittest.TestCase):
+class TestSolution(unittest.TestCase):
     def setUp(self):
         self.solution = Solution()
 
-    def test(self):
+    def test_example_case(self):
         temperatures = [73, 74, 75, 71, 69, 72, 76, 73]
-        ans = [1, 1, 4, 2, 1, 1, 0, 0]
+        expected = [1, 1, 4, 2, 1, 1, 0, 0]
+        self.assertEqual(self.solution.dailyTemperatures(temperatures), expected)
 
-        res = self.solution.dailyTemperatures(temperatures)
-        self.assertEqual(res, ans)
+    def test_all_same_temperatures(self):
+        temperatures = [70, 70, 70, 70]
+        expected = [0, 0, 0, 0]
+        self.assertEqual(self.solution.dailyTemperatures(temperatures), expected)
+
+    def test_decreasing_temperatures(self):
+        temperatures = [80, 79, 78, 77]
+        expected = [0, 0, 0, 0]
+        self.assertEqual(self.solution.dailyTemperatures(temperatures), expected)
+
+    def test_increasing_temperatures(self):
+        temperatures = [60, 61, 62, 63]
+        expected = [1, 1, 1, 0]
+        self.assertEqual(self.solution.dailyTemperatures(temperatures), expected)
+
+    def test_single_temperature(self):
+        temperatures = [50]
+        expected = [0]
+        self.assertEqual(self.solution.dailyTemperatures(temperatures), expected)
+
+    def test_alternating_temperatures(self):
+        temperatures = [70, 75, 70, 75, 70, 75]
+        expected = [1, 0, 1, 0, 1, 0]
+        self.assertEqual(self.solution.dailyTemperatures(temperatures), expected)
 
 
 if __name__ == "__main__":
