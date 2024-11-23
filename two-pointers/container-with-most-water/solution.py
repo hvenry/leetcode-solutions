6b826cd7a9f11e6fe373f4294a1e4cef6a254f5b
hvenry <hvendittelli@gmail.com>
2024-11-23 14:02:20 -0500

    Revision: 11. container-with-most-water

--- two-pointers/container-with-most-water/solution.py
+++ two-pointers/container-with-most-water/solution.py
@@ -1,44 +1,51 @@
-from typing import List
-
 """
-you are given an integer array height of length n. There are n vertical lines drawn
-such that two endpoints of the ith line are (i, 0) and (i, height[i]). (bottom of x axis to height value)
+You are given an integer array height of length n. There are n vertical lines drawn such the two endpoints
+of the ith line are (i, 0) and (i, height[i])).
 
-find the two lines that together with the x-axis form a container, such that it contains the most
-water.
+Find the two lines that together with the x-axis form a container, such that it contains the most water.
 
-example:
-
-
+Example:
 height = [1, 8, 6, 2, 5, 4, 8, 3, 7]
+
+  |         #
+  |         #   |
+  | #       #   |
+  | #   #   #   |
+  | #   # # #   |
+  | #   # # # # |
+  | # # # # # # |
+# | # # # # # # |
+  ^             ^
+
 ans = 49
 
-this is because 7 * 7 = 49, it takes the two walls, height[1] = 8, and height[8] = 7
+This is because 7 * 7 = 49, it takes the two walls, height[1] = 8, and height[8] = 7.
 
+Solution:
+- Use two-pointers, left starting at start at height[0] and right starting at height[len(height) - 1]
+- find the width between both walls (right - left), and multiple by the min of left and right heights to get the current area
+- update max area accordingly
+- move left or right depending on which wall is shorter
 
-
-solution:
-use two-pointers approach,
-- start at height[0] and height[len(height) - 1]
-- calculate the min of both ^2
-- move the pointer up to the next largest wall ->
-
-
+Complexity
+- Time: O(n)
+- Space: O(1) -> we do not store anything for this solution
 """
+
+from typing import List
 
 
 class Solution:
     def maxArea(self, height: List[int]) -> int:
+        n = len(height)
         left = 0
-        right = len(height) - 1
+        right = n - 1
         max_area = 0
 
         while left < right:
             width = right - left
             current_area = min(height[left], height[right]) * width
-
-            if current_area > max_area:
-                max_area = current_area
+            max_area = max(max_area, current_area)
 
             if height[left] < height[right]:
                 left += 1
--- two-pointers/container-with-most-water/test_solution.py
+++ two-pointers/container-with-most-water/test_solution.py
@@ -2,16 +2,37 @@ import unittest
 from solution import Solution
 
 
-class testSolution(unittest.TestCase):
+class TestSolution(unittest.TestCase):
     def setUp(self):
         self.solution = Solution()
 
-    def test(self):
+    def test_example_case(self):
         height = [1, 8, 6, 2, 5, 4, 8, 3, 7]
-        ans = 49
+        self.assertEqual(self.solution.maxArea(height), 49)
 
-        res = self.solution.maxArea(height)
-        self.assertEqual(res, ans)
+    def test_single_element(self):
+        height = [1]
+        self.assertEqual(self.solution.maxArea(height), 0)
+
+    def test_two_elements(self):
+        height = [1, 2]
+        self.assertEqual(self.solution.maxArea(height), 1)
+
+    def test_decreasing_heights(self):
+        height = [5, 4, 3, 2, 1]
+        self.assertEqual(self.solution.maxArea(height), 6)
+
+    def test_increasing_heights(self):
+        height = [1, 2, 3, 4, 5]
+        self.assertEqual(self.solution.maxArea(height), 6)
+
+    def test_same_heights(self):
+        height = [3, 3, 3, 3, 3]
+        self.assertEqual(self.solution.maxArea(height), 12)
+
+    def test_large_input(self):
+        height = [i for i in range(1, 10001)]
+        self.assertEqual(self.solution.maxArea(height), 25000000)
 
 
 if __name__ == "__main__":
