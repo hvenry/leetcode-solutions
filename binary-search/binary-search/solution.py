5c838cd6ffc21844856fffe59025ad567f54185b
hvenry <hvendittelli@gmail.com>
2024-12-08 18:53:01 -0500

    Revision: 704. binary-search

--- binary-search/binary-search/solution.py
+++ binary-search/binary-search/solution.py
@@ -1,19 +1,25 @@
+"""
+Given an array fo integers 'nums' which is sorted in ascending order, and an integer 'target', write a function to search
+'target' in 'nums'. If 'target' exists, then return its index. otherwise, return -1.
+
+Example:
+nums = [-1, 0, 3, 5, 9, 12]
+target = 9
+returns: 4
+
+Solution:
+- Binary search
+- use left and right to mark our search space
+- if target is greater than middle, update left to middle
+- if target is less tha middle, update right midle
+- if target is equal to middle, return middle
+
+Complexity:
+- Time: O(log n)
+- Space: O(1)
+"""
+
 from typing import List
-
-
-"""
-given an array fo integers 'nums' which is sorted in ascending order, and
-an integer 'target', write a function to search 'target' in 'nums'.
-
-if 'target' exists, then return its index. otherwise, return -1.
-
-to implement this with O (log n) time complexity, use binary search.
-
-
-how to implement
-- if target is smaller than mid, search lef
-- if target is larger than mid, search right
-"""
 
 
 class Solution:
@@ -24,13 +30,10 @@ class Solution:
         while left <= right:
             middle = (left + right) // 2
 
-            # check if target is at middle
             if target == nums[middle]:
                 return middle
-            # if target is smaller, ignore right half (set right to be middle THIS MOVES RIGHT HALFWAY INWARD)
             elif target < nums[middle]:
                 right = middle - 1
-            # if target is greater, ignore left half (set left to be middle THIS MOVES LEFT HALFWAY INWARD)
             else:
                 left = middle + 1
 
